3fe9599aeb78a896645043e6312bdefddb4c5818
Zubaer-ahamad <zunaedshibli26@gmail.com>
2024-04-21 18:20:43 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const hamBtn = document.getElementById('ham-button');
+//     const showItem = document.getElementById('hidden-icon');
+
+//     hamBtn.addEventListener('click', () => {
+//         showItem.style.setProperty('display', 'block');
+//     });
+// })
